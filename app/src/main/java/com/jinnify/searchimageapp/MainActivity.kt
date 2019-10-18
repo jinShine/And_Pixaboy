3066e976988e81da31df005fbf85e7b7423a8036
seungjin <seungjin429@naver.com>
2019-10-18 13:20:17 +0900

    RecyclerView 추가

--- app/src/main/java/com/jinnify/searchimageapp/MainActivity.kt
+++ app/src/main/java/com/jinnify/searchimageapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.jinnify.searchimageapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
