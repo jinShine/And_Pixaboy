c4f17af038e42a55695a37588aaa60377387afd9
seungjin <seungjin429@naver.com>
2019-10-21 19:05:27 +0900

    ViewModel 추가

--- app/src/main/java/com/jinnify/searchimageapp/SearchActivity.kt
+++ app/src/main/java/com/jinnify/searchimageapp/SearchActivity.kt
@@ -1,4 +1,39 @@
 package com.jinnify.searchimageapp
 
-class SearchActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.jinnify.searchimageapp.parser.PixaboyParser
+import kotlinx.android.synthetic.main.activity_search.*
+
+class SearchActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        @Suppress("UNCHECKED_CAST")
+        val viewModel = ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return SearchViewModel(PixaboyRepositoryImpl(PixaboyParser())) as T
+            }
+        }).get(SearchViewModel::class.java)
+
+        SearchButton.setOnClickListener {
+            println("")
+            val searchText = searchEditText.text.toString()
+            viewModel.searchImageFrom(searchText)
+        }
+
+        viewModel.searchedImageList.observe(this, Observer {
+            println("" + it)
+        })
+
+    }
 }
--- app/src/main/java/com/jinnify/searchimageapp/SearchViewModel.kt
+++ app/src/main/java/com/jinnify/searchimageapp/SearchViewModel.kt
@@ -1,4 +1,31 @@
 package com.jinnify.searchimageapp
 
-class SearchViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel(private val pixaboyRepository: PixaboyRepository): ViewModel() {
+
+    private val _searchedImageList = MutableLiveData<List<String>>()
+    val searchedImageList: LiveData<List<String>> = _searchedImageList
+
+    //Input
+    fun searchImageFrom(text: String) {
+        pixaboyRepository.fetchSearchImageFrom(text)
+    }
+
+    //Output
+    private val liveDataManager = Observer<List<String>> { imageList ->
+        _searchedImageList.value = imageList
+    }
+
+    init {
+        pixaboyRepository.searchedImageLiveData().observeForever(liveDataManager)
+    }
+
+    override fun onCleared() {
+        pixaboyRepository.searchedImageLiveData().removeObserver(liveDataManager)
+        super.onCleared()
+    }
 }
