7ee7b30aa1f5f7b9aae6f48d8c1268d376381ae0
seungjin <seungjin429@naver.com>
2019-10-21 19:05:15 +0900

    Pixaboy Repository 구현

--- app/src/main/java/com/jinnify/searchimageapp/PixaboyRepository.kt
+++ app/src/main/java/com/jinnify/searchimageapp/PixaboyRepository.kt
@@ -1,4 +1,28 @@
 package com.jinnify.searchimageapp
 
-class PixaboyRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.jinnify.searchimageapp.parser.PixaboyParser
+import kotlin.concurrent.thread
+
+interface PixaboyRepository {
+
+    fun searchedImageLiveData(): LiveData<List<String>>
+    fun fetchSearchImageFrom(text: String)
+}
+
+class PixaboyRepositoryImpl(private val pixaboyParser: PixaboyParser) : PixaboyRepository {
+
+    private val pixaboyLiveData = MutableLiveData<List<String>>()
+
+    override fun searchedImageLiveData() = pixaboyLiveData
+
+    override fun fetchSearchImageFrom(text: String) {
+        thread {
+            Thread.sleep(1000L)
+            val searchedImage = pixaboyParser.searchImageFrom(text)
+            pixaboyLiveData.postValue(searchedImage)
+        }
+    }
+
 }
